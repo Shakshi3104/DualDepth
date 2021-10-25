6ca05ebaf5239e2ab4fb70f3ef15557dbd54960e
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-10-25 17:17:26 +0900

    Initial Commit

---
+++ DualDepth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DualDepth
+//
+//  Created by MacBook Pro M1 on 2021/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DualDepth/DualDepthApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualDepthApp.swift
+//  DualDepth
+//
+//  Created by MacBook Pro M1 on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DualDepthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
